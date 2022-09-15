e35b315b70cae042e180f70a39a9c1ada1a60b0f
Ciro Ibarra V <ciro.ibarrav@gmail.com>
2022-09-15 10:11:02 -0500

    px replaced by rem and em, category icon added

--- src/components/RecentTransactions/TransactionCard.jsx
+++ src/components/RecentTransactions/TransactionCard.jsx
@@ -4,10 +4,18 @@ import "../RecentTransactions/RecentTransactions.css";
 export default function TransactionCard(props) {
   return (
     <div className="transaction-card">
-      <img alt="category icon" src="" className="transaction-categoryIcon" />
-      <h3 className="transaction-title">{props.title}</h3>
-      <div className="transaction-value">{`$ ${props.value}`}</div>
-      <p className="transaction-details">{props.details}</p>
+      <img
+        alt="category icon"
+        src="/assets/CategoryIcons/recurring-bill.svg"
+        className="transaction-categoryIcon"
+      />
+      <div className="transaction-card-details-container">
+        <div className="transaction-card-titleValue-container">
+          <h3 className="transaction-title">{props.title}</h3>
+          <div className="transaction-value">{`$ ${props.value}`}</div>
+        </div>
+        <p className="transaction-details">{props.details}</p>
+      </div>
     </div>
   );
 }
