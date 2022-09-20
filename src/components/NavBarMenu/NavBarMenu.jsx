01daceb277fda577fe9cf1abd3edc8f3bf5242cd
Ciro Ibarra V <ciro.ibarrav@gmail.com>
2022-09-20 06:54:22 -0500

    navBar modified

--- src/components/NavBarMenu/NavBarMenu.jsx
+++ src/components/NavBarMenu/NavBarMenu.jsx
@@ -4,33 +4,44 @@ import NavButtons from "./NavBarButtons";
 
 function NavBarMenu() {
   return (
-    <footer className="navBar">
+    <nav className="navBar">
+      <nav className="navBar-link-container">
+        <NavButtons buttonName={"Home"} />
+        <NavButtons buttonName={"Transaction"} />
+      </nav>
       <div className="navBar-action-btn-container">
         <ol className="navBar-action-btn">
           <li>
             <button>
-              <img alt="action icon 1" src=""></img>
+              <img
+                alt="action icon 1"
+                src="/assets/NavButtonsIcons/income.svg"
+              ></img>
             </button>
           </li>
           <li>
             <button>
-              <img alt="action icon 2" src=""></img>
+              <img
+                alt="action icon 2"
+                src="/assets/NavButtonsIcons/currency-exchange.svg"
+              ></img>
             </button>
           </li>
           <li>
             <button>
-              <img alt="action icon 3" src=""></img>
+              <img
+                alt="action icon 3"
+                src="/assets/NavButtonsIcons/expense.svg"
+              ></img>
             </button>
           </li>
         </ol>
       </div>
       <nav className="navBar-link-container">
-        <NavButtons buttonName={"Home"} />
-        <NavButtons buttonName={"Transaction"} />
         <NavButtons buttonName={"Budget"} />
         <NavButtons buttonName={"Profile"} />
       </nav>
-    </footer>
+    </nav>
   );
 }
 
